4cf9cdeb5fb5574bbe6ec1c3f00956b817abae91
kaylobadboy <kristopher.r.lommen@my.und.edu>
2017-05-27 14:25:25 -0500

    Admin account box+ crude CSS for admin menus

--- src/app/pages/admin-panel-base/admin-panel.component.ts
+++ src/app/pages/admin-panel-base/admin-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {LoginService} from '../../services/login.service';
 
 @Component({
   selector: 'app-admin-panel',
@@ -8,7 +9,7 @@ import {Router} from "@angular/router";
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public LoginService: LoginService) { }
 btnClick1() {
   this.router.navigateByUrl('/Admin/EditHome');
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -12,32 +12,18 @@ export class LoginService {
   headers: Headers;
   token: string = null;
   user: Observable<firebase.User>;
-
   constructor(public afAuth: AngularFireAuth) {
     this.user = afAuth.authState;
   }
+getCurrentUser() {
+  return  firebase.auth().currentUser.email;
+}getCurrentUserShortened() {
+   return firebase.auth().currentUser.email.substring(0, firebase.auth().currentUser.email.indexOf('@'));
+  }
   register(email: string, password: string) {
     console.log(email + password);
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
       console.log('Registration Successfull');
-    })
-
-
-
-      .catch(function(error) {
-        console.log(error);
-
-
-      });
-
-  }
-  loggedin() {
-    return (firebase.auth().currentUser) ? true : false;
-  }
-  login(email: string, password: string) {
-    console.log(email + password);
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
-      console.log('Login Successfull');
     })
 
 
@@ -46,7 +32,31 @@ export class LoginService {
 
 
       });
+
   }
+
+  loggedin() {
+    return (firebase.auth().currentUser) ? true : false;
+  }
+
+  login(email: string, password: string) {
+    console.log(email + password);
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
+
+      console.log(firebase.auth().currentUser);
+      console.log(firebase.auth().currentUser.email);
+      console.log('Login Successfull');
+
+    })
+
+
+      .catch(function (error) {
+        console.log(error);
+        alert(error);
+
+      });
+  }
+
     logout() {
 
       firebase.auth().signOut().then(function() {
@@ -55,6 +65,8 @@ export class LoginService {
         console.error('Sign Out Error', error);
       });
       }
-
+  onUserStateChange(user: AngularFireAuth) {
+    console.log("user state changed " + user);
+  }
 
 }
